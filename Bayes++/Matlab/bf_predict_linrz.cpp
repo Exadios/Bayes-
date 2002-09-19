abb72b558a4ee10a57ce32cdfd9902d64425db8e
mistevens <mistevens@b1e43ced-4510-0410-a874-8e9050bc1126>
2002-09-19 02:58:10 +0000

    toward 2002.10
    git-svn-id: https://bayesclasses.svn.sourceforge.net/svnroot/bayesclasses/trunk@10 b1e43ced-4510-0410-a874-8e9050bc1126

---
+++ Bayes++/Matlab/bf_predict_linrz.cpp
@@ -0,0 +1,90 @@
+/*
+ * Bayes++ the Bayesian Filtering Library
+ * Copyright (c) 2002 Michael Stevens, Australian Centre for Field Robotics
+ * See Bayes++.htm for copyright license details
+ */
+
+/*
+ * MATLAB MEX interface for Bayes++
+ *  bf_predict_linrz:  Predict filter through non-linear f(x) with Gq addative noise
+ */
+
+#include <mex.h>
+#include "bfilter.h"
+
+using namespace::Matlab_convert;
+
+
+void mexFunction(
+		 int          nlhs,
+		 mxArray      *plhs[],
+		 int          nrhs,
+		 const mxArray *prhs[]
+		 )
+{
+	/* Check for proper usage */
+	bool usageOk = true;
+	if (nrhs != 5)
+		usageOk = false;
+	if (nlhs > 1)
+		usageOk = false;
+
+	if (!usageOk)
+		mexErrMsgTxt("usage: [ <rcond>= ] predict_linrz(<handle>, <f_function>, <Fx>, <G>, <q>)");
+
+	// Check handle and get filter pointer
+	Run_Filter* filter = Run_Filter::from_handle(prhs[0]);
+
+	// Need filter state size
+	Bayesian_filter::State_filter* f = dynamic_cast<Bayesian_filter::State_filter*>(filter);
+	Run_Filter::check_def(f);
+
+	// Must catch exceptions for Matlab kernel
+	try
+	{
+		// Arguments
+		Matlab_function_model matlab_f(prhs[1]);
+		FM::Matrix Fx = Matrix(prhs[2]);
+		FM::Matrix G = Matrix(prhs[3]);
+		FM::Vec    q = Vector(prhs[4]);
+		// Check matrix conformance
+		if (f->x.size() != Fx.size1())
+			mexErrMsgTxt("Mismatch in x and Fx matrix");
+		if (Fx.size1() != Fx.size2())
+			mexErrMsgTxt("Mismatch Fx not square");
+		if (f->x.size() != G.size1())
+			mexErrMsgTxt("Mismatch in x and G size");
+		if (q.size() != G.size2())
+			mexErrMsgTxt("Mismatch in q and G size");
+
+		// Predict model
+		Bayesian_filter::Simple_linrz_predict_model model(matlab_f, Fx, G, q);
+char warn[1000];
+const FM::Vec& fx = matlab_f.fx(f->x);
+sprintf(warn, "x%d, fx%d, Fx%dX%d, G%dX%d, q%d", f->x.size(), fx.size(), Fx.size1(),Fx.size2(), G.size1(),G.size2(), q.size());
+mexWarnMsgTxt(warn);
+
+		bool bFilterOp = false;			// Flag operation complete
+		double rcond;
+		if (Bayesian_filter::Linrz_filter* f = dynamic_cast<Bayesian_filter::Linrz_filter*>(filter)) {
+			bFilterOp = true;
+			rcond = f->predict (model);
+		}
+				
+		if (bFilterOp) {
+			if (nlhs == 1)		// Optional return value
+				plhs[0] = Array(rcond);
+		}
+		else {
+			Run_Filter::check_def(NULL);
+		}
+	}
+	catch (std::exception& se)
+	{
+		mexErrMsgTxt( se.what() );
+	}
+	catch (...)
+	{
+		mexErrMsgTxt( "Filter operation caused an unknown exception" );
+	}
+}
