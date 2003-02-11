c97024d762eaa388bdaeaf72242cb1c051b8b581
mistevens <mistevens@b1e43ced-4510-0410-a874-8e9050bc1126>
2003-02-11 11:00:55 +0000

    Back to correct version
    git-svn-id: https://bayesclasses.svn.sourceforge.net/svnroot/bayesclasses/trunk@107 b1e43ced-4510-0410-a874-8e9050bc1126

--- Bayes++/Simple/simpleExample.cpp
+++ Bayes++/Simple/simpleExample.cpp
@@ -7,8 +7,7 @@
  */
 
 /*
- * Example of using Bayesian Filter Class
- *  Demonstrate the effect of observation decorrelation for linear models
+ * Example of using Bayesian Filter Class to solve a simple problem.
  *  A linear filter with one state and constant noises
  */
 
@@ -23,9 +22,9 @@ using namespace Bayesian_filter_matrix;
 
 
 /*
- * Observation model: Two uncorrelated observations
+ * Simple Prediction model
  */
-class Uncorrelated_observe : public Linear_uncorrelated_observe_model
+class Simple_predict : public Linear_predict_model
 {
 public:
 	Simple_predict() : Linear_predict_model(1,1)
@@ -34,7 +33,7 @@ public:
 				// Stationary Prediction model (Identity)
 		Fx(0,0) = 1.;
 				// Constant Noise model with a large variance
-		q[0] = 2.0;
+		q[0] = 2.;
 		G(0,0) = 1.;
 	}
 };
@@ -45,15 +44,13 @@ public:
 class Simple_observe : public Linear_uncorrelated_observe_model
 {
 public:
-	Simple_observe () :	Linear_uncorrelated_observe_model(1,1)
+	Simple_observe () : Linear_uncorrelated_observe_model(1,1)
 	// Construct a constant model
 	{
 				// Linear model
 		Hx(0,0) = 1;
-		Hx(1,0) = 2;
 				// Constant Observation Noise model with variance of one
 		Zv[0] = 1.;
-		Zv[1] = 1.;
 	}
 };
 
@@ -66,19 +63,26 @@ int main()
 	// Global setup for test output
 	cout.flags(ios::fixed); cout.precision(4);
 
-	// Construct Observation models
-	Uncorrelated_observe my_observe;
+	// Construct simple Prediction and Observation models
+	Simple_predict my_predict;
+	Simple_observe my_observe;
 
 	// Use an 'unscented' filter with one state
 	Unscented_filter my_filter(1);
 
 	// Setup the initial state and covariance
 	Vec x_init (1);	SymMatrix X_init (1, 1);
-	x_init[0] = 1.;			// Start with some uncertainty
-	X_init(0,0) = 5.;
+	x_init[0] = 10.;		// Start at 10 with no uncertainty
+	X_init(0,0) = 0.;
 	my_filter.init_kalman (x_init, X_init);
 
 	cout << "Initial  " << my_filter.x << my_filter.X << endl;
+
+	// Predict the filter forward
+	my_filter.predict (my_predict);
+	my_filter.update();		// Update the filter, so state and covariance are available
+
+	cout << "Predict  " << my_filter.x << my_filter.X << endl;
 
 	// Make an observation
 	Vec z(1);
